effd5779c063329babf43835203a2d8bd9ec5a28
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-03-06 13:29:06 +0900

    fix_require

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,6 @@
+require 'rss'
+
 class FeedsController < ApplicationController
-  require 'rss'
   def index
     url = "https://news.yahoo.co.jp/pickup/computer/rss.xml"
     @feed = RSS::Parser.parse(url)
