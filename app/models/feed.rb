abc3e61b11902e698a68a28a9049732ca60e9eab
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-03-13 18:20:06 +0900

    バリデーション修正

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,5 @@
 class Feed < ApplicationRecord
-  validates :url, format: /\A#{URI::regexp(%w(http https))}\z/, presence: true
+  validates :url, format: /\A#{URI::regexp(%w(http https))}\z/
 
   def self.channels
     all.map { |feed| RSS::Parser.parse(feed.url).channel }
