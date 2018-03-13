bcf5b6b0f6246f50df02e60f18d67288fe024d9a
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-03-13 18:06:30 +0900

    test内の不要なpostfix削除

--- test/models/feed_test.rb
+++ test/models/feed_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class FeedTest < ActiveSupport::TestCase
-  
+
   test "should save feed with valid url" do
     feed = Feed.new(url: "https://rss-weather.yahoo.co.jp/rss/days/7320.xml")
     assert feed.valid?
   end
 
   test "should not save feed when scheme is not http/https" do
-    feed_false = Feed.new(url: "javascript:alert('XSS');//http://shiraishi.jp/")
-    assert feed_false.invalid?
+    feed = Feed.new(url: "javascript:alert('XSS');//http://shiraishi.jp/")
+    assert feed.invalid?
   end
 
   test "should not save feed without url" do
-    feed_blank = Feed.new
-    assert feed_blank.invalid?
+    feed = Feed.new
+    assert feed.invalid?
   end
 end
