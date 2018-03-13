c46e1587e04cbe8fcbabe8212163527a6e23692a
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-03-13 15:44:13 +0900

    test内のコード修正

--- test/models/feed_test.rb
+++ test/models/feed_test.rb
@@ -1,27 +1,13 @@
 require 'test_helper'
 
 class FeedTest < ActiveSupport::TestCase
-  def feed_true
-    @feed_true ||= Feed.new(
-    url: "https://rss-weather.yahoo.co.jp/rss/days/7320.xml"
-    )
+  test  "url validate" do
+    feed_true = Feed.new(url: "https://rss-weather.yahoo.co.jp/rss/days/7320.xml")
+    feed_false = Feed.new(url: "javascript:alert('XSS');//http://shiraishi.jp/")
+    feed_blank = Feed.new
 
-  end
-
-  def feed_false
-    @feed_false ||= Feed.new(
-    url: "javascript:alert('XSS');//http://shiraishi.jp/"
-    )
-    @feed_false ||= Feed.new(
-    url: ""
-    )
-  end
-
-  def test_valid
     assert feed_true.valid?
-  end
-
-  def test_invalid
     assert feed_false.invalid?
+    assert_not feed_blank.save
   end
 end
