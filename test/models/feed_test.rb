5e3f8e05d066254a6259b43bacef86e46fec54dc
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-03-13 17:51:04 +0900

    test内のコード分け

--- test/models/feed_test.rb
+++ test/models/feed_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class FeedTest < ActiveSupport::TestCase
-  test  "url validate" do
-    feed_true = Feed.new(url: "https://rss-weather.yahoo.co.jp/rss/days/7320.xml")
-    feed_false = Feed.new(url: "javascript:alert('XSS');//http://shiraishi.jp/")
-    feed_blank = Feed.new
+  
+  test "should save feed with valid url" do
+    feed = Feed.new(url: "https://rss-weather.yahoo.co.jp/rss/days/7320.xml")
+    assert feed.valid?
+  end
 
-    assert feed_true.valid?
+  test "should not save feed when scheme is not http/https" do
+    feed_false = Feed.new(url: "javascript:alert('XSS');//http://shiraishi.jp/")
     assert feed_false.invalid?
-    assert_not feed_blank.save
+  end
+
+  test "should not save feed without url" do
+    feed_blank = Feed.new
+    assert feed_blank.invalid?
   end
 end
