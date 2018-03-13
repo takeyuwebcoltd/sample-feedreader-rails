8702a8d6a06ebf1c89d7b68cd2937023234fa9fc
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-03-13 12:26:30 +0900

    test追加

--- test/models/feed_test.rb
+++ test/models/feed_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class FeedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def feed_true
+    @feed ||= Feed.new(
+    url: "https://rss-weather.yahoo.co.jp/rss/days/7320.xml"
+    )
+
+  end
+
+  def feed_false
+    @feed ||= Feed.new(
+    url: "javascript:alert('XSS');//http://shiraishi.jp/"
+    )
+    @feed_blank ||= Feed.new(
+    url: ""
+    )
+  end
+
+  def test_valid
+    assert feed_true.valid?
+  end
+
+  def test_invalid
+    assert feed_false.invalid?
+  end
 end
