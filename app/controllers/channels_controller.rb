c3544d43ad6533a418fb1b333cbe9ce5692ef050
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-03-30 06:06:55 +0900

    fetch_items内の更新を検知する

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,5 +1,5 @@
 require 'rss'
-# https://news.yahoo.co.jp/pickup/computer/rss.xml
+# https://news.yahoo.co.jp/pickup/rss.xml
 # https://rss-weather.yahoo.co.jp/rss/days/7320.xml
 class ChannelsController < ApplicationController
   def index
@@ -20,31 +20,27 @@ class ChannelsController < ApplicationController
   end
 
   def fetch_items
-
     channel = Channel.find(params[:channel_id])
-    fetch_channel = RSS::Parser.parse(channel.url).channel
+    source_channel = RSS::Parser.parse(channel.url).channel
 
-    channel.title = fetch_channel.title
-    channel.description = fetch_channel.description
-    channel.changed?
-      channel.save
+    channel.assign_attributes(title: source_channel.title, description: source_channel.description)
+    channel.save if channel.changed?
 
-    if channel.items.first == nil
-      fetch_channel.items.each.map do |fetch_item|
-        item = channel.items.build
-        item.title = fetch_item.title
-        item.link = fetch_item.link
-        item.pubdate = fetch_item.pubDate
-        item.save
+    if channel.items.last
+      item = channel.items.order(:pubdate).last
+      source_items = source_channel.items.sort!{ |a,b| b.date <=> a.date }
+      source_item = source_items.first
+      item.title = source_item.title
+      #item.assign_attributes(title: source_item.title, link: source_item.link, pubdate: sou  rce_item.pubDate)
+      if item.changed?
+        source_channel.items.each do |source_item|
+          item.update(title: source_item.title, link: source_item.link, pubdate: source_item.pubDate)
+        end
       end
     else
-      item = channel.items.first
-      fetch_item = fetch_channel.items.first
-        item.title = fetch_item.title
-        item.link = fetch_item.link
-        item.pubdate = fetch_item.pubDate
-        item.changed?
-          item.save
+      source_channel.items.each do |source_item|
+        channel.items.create(title: source_item.title, link: source_item.link, pubdate: source_item.pubDate)
+      end
     end
     redirect_to channels_path
   end
