006f2a9fbb36df52994c4953a77e0e50ba7751d7
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-04-08 03:12:05 +0900

    create & fetch_items内の記述を修正

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,34 +2,37 @@ require 'rss'
 # https://news.yahoo.co.jp/pickup/rss.xml
 # https://rss-weather.yahoo.co.jp/rss/days/7320.xml
 class ChannelsController < ApplicationController
+  before_action :set_channel,only:[:new, :fetch_items]
+
   def index
     @channels = Channel.all
   end
 
   def new
-    @channel = Channel.new
   end
 
   def create
     @channel = Channel.new(channel_params)
     if @channel.save
+      @channel.items_save(@channel.id)
       redirect_to channels_path
     else
       render 'new'
     end
-    channel_id = @channel.id
-    Channel.items_save(channel_id)
   end
 
   def fetch_items
-    channel_id = params[:channel_id]
-    Channel.items_save(channel_id)
+    @channel.items_save(params[:channel_id])
     redirect_to channels_path
   end
 
   private
   def channel_params
     params.require(:channel).permit(:url)
+  end
+
+  def set_channel
+    @channel = Channel.new
   end
 
 end
