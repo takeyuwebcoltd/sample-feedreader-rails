69e006169cf766dfd9c0e9eed831b86eff57b7b3
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-04-17 02:56:01 +0900

    items_saveメソッド修正（create, fetch_itemsアクション内も）

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,19 +2,19 @@ require 'rss'
 # https://news.yahoo.co.jp/pickup/rss.xml
 # https://rss-weather.yahoo.co.jp/rss/days/7320.xml
 class ChannelsController < ApplicationController
-  before_action :set_channel,only:[:new, :fetch_items]
 
   def index
     @channels = Channel.all
   end
 
   def new
+    @channel = Channel.new
   end
 
   def create
     @channel = Channel.new(channel_params)
     if @channel.save
-      @channel.items_save(@channel.id)
+      @channel.items_save
       redirect_to channels_path
     else
       render 'new'
@@ -22,17 +22,14 @@ class ChannelsController < ApplicationController
   end
 
   def fetch_items
-    @channel.items_save(params[:channel_id])
+    channel = Channel.find(params[:channel_id])
+    channel.items_save
     redirect_to channels_path
   end
 
   private
   def channel_params
     params.require(:channel).permit(:url)
-  end
-
-  def set_channel
-    @channel = Channel.new
   end
 
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,15 +1,15 @@
 class Channel < ApplicationRecord
+  attr_accessor :channel
 
   has_many :items, dependent: :destroy
   validates :url, format: /\A#{URI::regexp(%w(http https))}\z/
 
-  def items_save(channel_id)
-    channel = self.class.find(channel_id)
-    source_channel = RSS::Parser.parse(channel.url).channel
-    channel.assign_attributes(title: source_channel.title, description: source_channel.description)
-    channel.save if channel.changed?
+  def items_save
+    source_channel = RSS::Parser.parse(self.url).channel
+    self.assign_attributes(title: source_channel.title, description: source_channel.description)
+    self.save if self.changed?
 
-    items = channel.items
+    items = self.items
     source_channel.items.each do |source_item|
       item = items.find_or_initialize_by(title: source_item.title)
       item.assign_attributes(link: source_item.link, pubdate: source_item.pubDate)
